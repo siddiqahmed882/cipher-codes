45b8fa1df39d6161c7abe82f93ba62f854733090
siddiqahmed882 <siddiqahmed882@gmail.com>
2023-05-02 23:56:14 +0500

    code for caeser cipher is added

---
+++ CaesarCipher.java
@@ -0,0 +1,84 @@
+import java.util.Scanner;
+
+class CaesarCipher {
+	public static StringBuffer encrypt(String text, int shift) {
+		StringBuffer result = new StringBuffer();
+
+		for (int i = 0; i < text.length(); i++) {
+			// replace spaces with |
+			if (text.charAt(i) == ' ') {
+				result.append('|');
+				continue;
+			}
+			if (Character.isUpperCase(text.charAt(i))) {
+				char ch = (char) (((int) text.charAt(i) + shift - 65) % 26 + 65);
+				result.append(ch);
+			} else {
+				char ch = (char) (((int) text.charAt(i) + shift - 97) % 26 + 97);
+				result.append(ch);
+			}
+		}
+		return result;
+	}
+
+	public static StringBuffer decrypt(String text, int shift) {
+		StringBuffer result = new StringBuffer();
+
+		for (int i = 0; i < text.length(); i++) {
+			// replace | with spaces
+			if (text.charAt(i) == '|') {
+				result.append(' ');
+				continue;
+			}
+			if (Character.isUpperCase(text.charAt(i))) {
+				char ch = (char) (((int) text.charAt(i) - shift - 65 + 26) % 26 + 65);
+				result.append(ch);
+			} else {
+
+				char ch = (char) (((int) text.charAt(i) - shift - 97 + 26) % 26 + 97);
+				result.append(ch);
+			}
+		}
+		return result;
+
+	}
+
+	// Driver code
+	public static void main(String[] args) {
+		// write an infinite loop to keep the program running until the user exits
+		while (true) {
+			// create a scanner object to read the user input
+			Scanner sc = new Scanner(System.in);
+
+			// ask the user to enter the text to be encrypted
+			System.out.print("Enter the text to be encrypted: ");
+			String text = sc.nextLine();
+
+			// ask the user to enter the shift value
+			System.out.print("Enter the shift value: ");
+			int shift = sc.nextInt();
+
+			// encrypt the text
+			String encryptedText = encrypt(text, shift).toString();
+
+			// decrypt the text
+			String decryptedText = decrypt(encryptedText, shift).toString();
+
+			// print the encrypted text
+			System.out.println("Encrypted text: " + encryptedText);
+
+			// print the decrypted text
+			System.out.println("Decrypted text: " + decryptedText);
+
+			// ask the user if they want to continue
+			System.out.print("Do you want to continue? (Y/N) :");
+			String choice = sc.next();
+
+			// if the user enters N or n, exit the program
+			if (choice.equals("N") || choice.equals("n")) {
+				System.out.println("Exiting...");
+				break;
+			}
+		}
+	}
+}
